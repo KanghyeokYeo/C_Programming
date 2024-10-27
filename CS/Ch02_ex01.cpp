5462d333187481e20aca62f2975c6ec165105e1b
KanghyeokYeo <ykh5548@naver.com>
2024-10-27 17:40:29 +0900

    Chapter 02

---
+++ CS/Ch02_ex01.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+	int Minsoo = 5;
+	int Sumin = 8;
+	printf("민수와 수민이가 구입한 사탕의 개수는 %d개이다.", Minsoo + Sumin);
+	return 0;
+}
---
+++ CS/Ch02_ex02.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() {
+	int flower = 10;
+	printf("%d", flower);
+	return 0;
+}
