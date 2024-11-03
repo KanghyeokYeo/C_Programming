ce017d1e54fd50e94aaf614919f2b05abd680f00
KanghyeokYeo <ykh5548@naver.com>
2024-11-04 05:58:28 +0900

    241104 study

--- Project111/Main.cpp
+++ Project111/Main.cpp
@@ -1,12 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include <cstdlib>
+#include <ctime> 
+
 int main() {
-	int i = 0;
+	int a, b, c;
+	int flag = 0; // 반복 확인 조건
+	srand((unsigned int)time(NULL)); // 변화하는 시간값을 전달하는 함수
 
+	int count = 0;
 	do {
-		printf("안녕하세요\n");
-		i++;
-	} while (false); // 거짓이더라도 한번은 수행
+		count++;
+		a = rand() % 6 + 1;
+		b = rand() % 6 + 1;
+		c = rand() % 6 + 1;
+		
+		if (a == b && b == c) flag = 1;
+	} while (flag == 0);
 
+	printf("주사위는 총 %d번 던졌습니다.\n", count);
+	printf("주사위가 가리키는 수는 %d입니다.", a);
 	return 0;
 }
