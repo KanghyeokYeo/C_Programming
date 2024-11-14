d04991fb4bda1d4a538678638112556777ce7090
KanghyeokYeo <ykh5548@naver.com>
2024-11-14 09:07:31 +0900

    Study

--- Project111/Main.cpp
+++ Project111/Main.cpp
@@ -4,21 +4,6 @@
 #include <ctime> 
 
 int main() {
-	int a, b, c;
-	int flag = 0; // 반복 확인 조건
-	srand((unsigned int)time(NULL)); // 변화하는 시간값을 전달하는 함수
-
-	int count = 0;
-	do {
-		count++;
-		a = rand() % 6 + 1;
-		b = rand() % 6 + 1;
-		c = rand() % 6 + 1;
-		
-		if (a == b && b == c) flag = 1;
-	} while (flag == 0);
-
-	printf("주사위는 총 %d번 던졌습니다.\n", count);
-	printf("주사위가 가리키는 수는 %d입니다.", a);
+	
 	return 0;
 }
